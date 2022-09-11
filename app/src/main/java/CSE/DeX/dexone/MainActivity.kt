e9aea8e788f5b68ab961f24a2da4f072bc950fc8
Devesh Pandey <deveshp1001@gmail.com>
2022-09-11 13:14:39 +0530

    removed MediaPlayer

--- app/src/main/java/CSE/DeX/dexone/MainActivity.kt
+++ app/src/main/java/CSE/DeX/dexone/MainActivity.kt
@@ -1,6 +1,5 @@
 package CSE.DeX.dexone
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -12,13 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val img = findViewById<ImageView>(R.id.imageView)
-        var music:MediaPlayer
 
         Glide.with(this)
             .load(R.drawable.al404)
             .into(img)
 
-        music = MediaPlayer.create(this,R.raw.this_girl)
-        music?.start()
     }
 }
